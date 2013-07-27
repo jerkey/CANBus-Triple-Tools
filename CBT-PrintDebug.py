f6bbfeb72a9268203a35e1b409a29fd735eff59f
Brent T. Dukes <brentdukes@gmail.com>
2013-07-26 21:40:59 -0400

    Added debug tool

---
+++ CBT-PrintDebug.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+#
+# Call like this:
+# 
+# ./CBT-PrintDebug.py /dev/ttyACM0
+#
+#
+import sys
+import serial
+
+ser = serial.Serial(sys.argv[1], 115200, timeout=1)
+ser.open()
+ser.write('\x01')
+
+print('\n')
+print(ser.read(4096))	
+
+ser.close()
